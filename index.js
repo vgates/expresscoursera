19de1f0fd5ca1b8ce45d25ac548571d093f8362b
Vinod K <vinodk@jecc.ac.in>
2020-08-19 05:28:57 +0530

    Cookies

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,6 +13,8 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
+// Start DB
+// mongod --dbpath=data --bind_ip 127.0.0.1
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
@@ -21,6 +24,45 @@ connect.then((db) => {
 
 app.use(morgan('dev')); // for logging purposes
 app.use(bodyParser.json());
+
+app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth (req, res, next) {
+
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+}
+app.use(auth);
 
 app.use(express.static(__dirname + '/public'));
 
