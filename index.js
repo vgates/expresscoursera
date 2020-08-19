ae5f8a50a048bf0924e89bff365240a0776c40b6
Vinod K <vinodk@jecc.ac.in>
2020-08-19 19:32:23 +0530

    session completed

--- index.js
+++ index.js
@@ -35,42 +35,27 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     store: new FileStore()
-  }));
+}));
 
-  function auth (req, res, next) {
+const userRouter = require('./routes/userRouter');
+app.use('/users', userRouter);
+
+function auth(req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');                        
-            err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
-            next(); // authorized
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     }
     else {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ',req.session);
+        if (req.session.user === 'authenticated') {
             next();
         }
         else {
             var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
+            err.status = 403;
+            return next(err);
         }
     }
 }
@@ -87,6 +72,8 @@ app.use('/promotions', promoRouter);
 
 const leaderRouter = require('./routes/leaderRouter');
 app.use('/leaders', leaderRouter);
+
+
 
 const server = http.createServer(app);
 
